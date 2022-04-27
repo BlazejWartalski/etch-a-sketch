5aa43c7872a6723e12749bb4d72f8b99aaf364d8
Blazej <BlazejWartalski97@gmail.com>
2022-04-27 21:55:14 +0100

    no message

--- script.js
+++ script.js
@@ -1,53 +1,83 @@
 // Sets important constants and variables
 
-const container = document.getElementById("container");
+const gridContainer = document.getElementById("gridContainer");
+const button = document.getElementById("btn");
+const button2 = document.getElementById("btn2");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
+var boxes = document.getElementsByClassName("cell");
 
-// Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
+function customGrid(size) {
+    makeRows(size);
+    makeColumns(size);
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("mouseenter", myFunction2);
+    };
+};
 
 // Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
+function makeRows(size) {
 
     // Creates rows
-    for (r = 0; r < rowNum; r++) {
+    for (r = 0; r < size; r++) {
         let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
+        gridContainer.appendChild(row).className = "gridRow";
     };
 };
 
 // Creates columns
-function makeColumns(cellNum) {
+function makeColumns(size) {
     for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
+        for (j = 0; j < size; j++) {
             let newCell = document.createElement("div");
             rows[j].appendChild(newCell).className = "cell";
         };
-
     };
 };
-defaultGrid();
 
-var boxes = document.getElementsByClassName("cell");
+function resetSketchbox() {
+    let size = prompt("Please enter desired number of boxes", "Please enter a valid number");
+    intSize = parseInt(size);
+    console.log(typeof(intSize));
+    console.log(intSize);
+    if(size > 100) {
+        console.log("Too big.")
+    } else if(isNaN(intSize)) {
+        console.log(typeof intSize);
+        console.log("Please insert a number, without decimal spaces.");
+    } else {
+        var refreshGrid = document.getElementsByClassName("cell");
+        var refreshGridRow = document.getElementsByClassName("gridRow");
 
-// boxes.addEventListener("mouseover", myFunction);
-
-function myFunction(e) {
-    e.target.style.background = 'blue'
+        while(refreshGrid.length > 0){
+            refreshGrid[0].parentNode.removeChild(refreshGrid[0]);
+            refreshGridRow[0].parentNode.removeChild(refreshGridRow[0]);
+        }
+        customGrid(size);
+    }
 };
+
+resetSketchbox();
+
 
 function myFunction2(e) {
-    e.target.style.background = 'black'
+    e.target.classList.add('hovered');
 };
 
 for (var i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener("mouseover", myFunction);
-}
+    boxes[i].addEventListener("mouseenter", myFunction2);
+};
 
-for (var i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener("mouseleave", myFunction2);
-}
+function removeClass() {
+    console.log('fuck');
+    var allBoxes = document.getElementsByClassName("cell");
+    console.log(allBoxes);
+    for (let box of allBoxes) {
+        box.classList.remove("hovered");
+    }
+    // allBoxes.classList.remove("hovered");
+};
+
+button.addEventListener("click", removeClass);
+
+button2.addEventListener("click", resetSketchbox);
